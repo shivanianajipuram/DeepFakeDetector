ed0b0654187cca1c275fd9c6bf162e1ac89dbda5
shivanianajipuram <shivanianajipuram@gmail.com>
2026-05-26 01:24:26 +0530

    Updated

--- app.py
+++ app.py
@@ -1,41 +1,38 @@
+import os
 import gradio as gr
 import numpy as np
 from keras.models import load_model
 from keras.preprocessing import image
 
-model = load_model("deepfake_model.keras")
+print("Starting app...")
 
 IMG_SIZE = 128
+
+print("Loading model...")
+model = load_model("deepfake_model.keras")
+print("Model loaded successfully")
 
 
 def predict(img):
     try:
-        # Resize
         img = img.resize((IMG_SIZE, IMG_SIZE))
 
-        # Convert to array
         img_array = image.img_to_array(img)
-
-        # Normalize
         img_array = img_array / 255.0
-
-        # Batch dimension
         img_array = np.expand_dims(img_array, axis=0)
 
-        # Predict
         prediction = model.predict(img_array)[0][0]
-
-        print("Prediction Value:", prediction)
 
         if prediction > 0.5:
             return "REAL IMAGE"
-
         else:
             return "FAKE IMAGE"
 
     except Exception as e:
         return f"Error: {str(e)}"
 
+
+print("Creating Gradio interface...")
 
 app = gr.Interface(
     fn=predict,
@@ -45,9 +42,12 @@ app = gr.Interface(
     description="Upload an image to check whether it is REAL or FAKE."
 )
 
+print("Launching app...")
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 7860))
+
     app.launch(
         server_name="0.0.0.0",
-        server_port=7860
+        server_port=port
     )
